769d09f52df02b2bee4d4e87442cc2858d6d67b1
MrBarnesTVDSB <90469601+MrBarnesSDCI@users.noreply.github.com>
2021-12-03 09:06:03 -0500

    Add files via upload

---
+++ student.hpp
@@ -0,0 +1,28 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+string exerciseOne (string word, int number)
+{
+	return "0";//TODO: Fix this
+}
+
+string exerciseTwo (int number)
+{
+	return "0";//TODO: Fix this
+}
+
+string exerciseThree (int number)
+{
+	return "0";//TODO: Fix this
+}
+
+int exerciseFour (int number)
+{
+	return 0;//TODO: Fix this
+}
